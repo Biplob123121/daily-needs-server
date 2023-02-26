4ea229ab2fc5310f079f31ef582cb72895f1d921
Biplob123121 <biplobkb123@gmail.com>
2023-02-26 14:35:58 +0600

    daily needs server project inilialize

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const { json } = require('express');
+require('dotenv').config();
+
+const app = express();
+
+
+//middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/./views/index.html');
+});
+
+
+// Route not found
+app.use((req, res, next) => {
+    res.status(404).json({
+        message: "Route not Found..."
+    })
+})
+
+// server error
+app.use((err, req, res, next) => {
+    res.status(500).json({
+        message: "Something is wrong"
+    })
+})
+
+module.exports = app;
